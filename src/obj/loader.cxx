5b4bb0ce2e5b9563c609940a4eed9653ff3cbb6c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-05-23 11:17:16 -0300

    Created obj namespace.

---
+++ src/obj/loader.cxx
@@ -0,0 +1,15 @@
+
+#include "modelloader.h"
+#include "model.h"
+
+namespace ep2 {
+
+using std::string;
+
+Model::Ptr ModelLoader::load (const string& filename) {
+  return Model::Ptr();
+}
+
+} // namespace ep2
+
+
