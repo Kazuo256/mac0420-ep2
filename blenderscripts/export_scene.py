aaafb334536136fe04fc133e4c477d0838e7d962
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-06-07 21:21:47 -0300

    Fixed blender script.

--- blenderscripts/export_scene.py
+++ blenderscripts/export_scene.py
@@ -9,9 +9,14 @@ def write_some_data(context, filepath, use_some_setting):
             continue
         f.write("%s\n" % re.split("\.", k, 1)[0])
         for i in [0,2,1,3]:
-            f.write("%(0)f %(1)f %(2)f %(3)f\n" %\
-                    {'0':v.matrix_basis[0][i], '1':v.matrix_basis[1][i],\
-                     '2':v.matrix_basis[2][i], '3':v.matrix_basis[3][i]})
+            if i == 2:
+                f.write("%(0)f %(1)f %(2)f %(3)f\n" %\
+                        {'0':-v.matrix_basis[0][i], '1':-v.matrix_basis[1][i],\
+                         '2':-v.matrix_basis[2][i], '3':-v.matrix_basis[3][i]})
+            else:
+                f.write("%(0)f %(1)f %(2)f %(3)f\n" %\
+                        {'0':v.matrix_basis[0][i], '1':v.matrix_basis[1][i],\
+                         '2':v.matrix_basis[2][i], '3':v.matrix_basis[3][i]})
     f.close()
 
     return {'FINISHED'}
