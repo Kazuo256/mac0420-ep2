4b3fc89a8dddf63d274c0ae9f78d5ad28f50655c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-06-10 21:27:06 -0300

    Publiching first

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+outdir=Thiago-Wilson
+
+if [ ! -d $outdir ]; then
+  mkdir $outdir
+fi
+
+contents="src/ CMakeLists.txt LEIAME models/ textures/ ime.*"
+cp -r $contents $outdir
+
+tar -caf $outdir.tgz $outdir
+
+rm -rf $outdir
+
