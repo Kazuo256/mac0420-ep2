cd1eeedf0d653dad0d0c173fb40396605c215d8e
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-06-10 21:34:40 -0300

    Publishing...

--- publish.sh
+++ publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-outdir=Thiago-Wilson
+outdir=ep2-Thiago-Wilson
 
 if [ ! -d $outdir ]; then
   mkdir $outdir
 fi
 
-contents="src/ CMakeLists.txt LEIAME models/ textures/ ime.*"
+contents="src/ CMakeLists.txt LEIAME models/*.obj models/*.mtl textures/*.png textures/*.jpg ime.*"
 cp -r $contents $outdir
 
 tar -caf $outdir.tgz $outdir
