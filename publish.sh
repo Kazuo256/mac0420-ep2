1560b64247b056231876a500fbe94254a930f3ad
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-06-10 21:39:17 -0300

    Publish!

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ if [ ! -d $outdir ]; then
   mkdir $outdir
 fi
 
-contents="src/ CMakeLists.txt LEIAME models/*.obj models/*.mtl textures/*.png textures/*.jpg ime.*"
+contents="src/ CMakeLists.txt LEIAME models/ textures/ ime.*"
 cp -r $contents $outdir
 
 tar -caf $outdir.tgz $outdir
